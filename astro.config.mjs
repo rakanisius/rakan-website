da3724738409d462fb0338505fa684fe219534c5
Rakan <rakanisius@gmail.com>
2026-09-24 11:28:12 +0700

    Tambahkan SEO dasar sitemap dan robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rakan.my.id',
+
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404')
+    })
+  ],
+
   adapter: cloudflare()
 });
